6657ecee9fc5c9f01fd709f7962b559301827982
AlessiaCaccamo <118570185+AlessiaCaccamo@users.noreply.github.com>
2025-10-02 16:13:45 +0100

    Update README.m

--- README.m
+++ README.m
@@ -16,4 +16,3 @@ Please note that the DIP_DCM toolbox can be found here: https://github.com/Aless
 
 ## Citing and using the code
 If you use the DIP-DCM toolbox, please cite the above paper. The DIP-DCM toolbox code is open source, under the terms of the GNU General Public License. This works on Windows, Linux, and macOS with an installed version of SPM. The code was originally used with SPM12 only. Code includes third-party functions (SPM, https://www.fil.ion.ucl.ac.uk/spm), with their respective copyright. 
-
